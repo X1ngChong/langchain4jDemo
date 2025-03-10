a202836a5dae9079fca53f5e219cfee167e2dda4
xingChong2 <13686426+xingchong2@user.noreply.gitee.com>
2025-03-10 19:24:20 +0800

    langchang4j Test

---
+++ src/test/java/top/bhui/lanchain4jdemo/Lanchain4jDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.bhui.lanchain4jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lanchain4jDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
